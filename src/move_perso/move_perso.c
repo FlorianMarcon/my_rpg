c05ade0d598332cd76c7a854964b666e4de5178a
paulvitry <paul.vitry@epitech.eu>
2018-05-09 10:52:14 +0200

    vitesse perso

--- src/move_perso/move_perso.c
+++ src/move_perso/move_perso.c
@@ -27,13 +27,13 @@ void	move_y(perso_t *perso, my_clock_t *my_clock)
 {
 	if (perso->mouvement.y == 1) {
 		move_rect(perso, 3, my_clock->second, my_clock->my_clock);
-		perso->position.y -= 0.72;
-		perso->position.x -= 1.82;
+		perso->position.y -= 1;
+		perso->position.x -= 2.52;
 	}
 	if (perso->mouvement.y == -1) {
 		move_rect(perso, 0, my_clock->second, my_clock->my_clock);
-		perso->position.y += 0.72;
-		perso->position.x += 1.82;
+		perso->position.y += 1;
+		perso->position.x += 2.52;
 	}
 }
 
@@ -43,13 +43,13 @@ void	move_perso(perso_t *perso, my_clock_t *my_clock)
 	move_y(perso, my_clock);
 	if (perso->mouvement.x == -1) {
 		move_rect(perso, 1, my_clock->second, my_clock->my_clock);
-		perso->position.x -= 1.3;
-		perso->position.y += 0.9;
+		perso->position.x -= 2.016;
+		perso->position.y += 1.4;
 	}
 	if (perso->mouvement.x == 1) {
 		move_rect(perso, 2, my_clock->second, my_clock->my_clock);
-		perso->position.x += 1.3;
-		perso->position.y -= 0.9;
+		perso->position.x += 2.016;
+		perso->position.y -= 1.4;
 	}
 	sfSprite_setPosition(perso->sprite, perso->position);
 }
